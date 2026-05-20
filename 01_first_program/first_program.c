8ba6dbf04eccf3dfafc19f664ee4f49eedd55c3c
Ayang1569 <ayang6248@floridapoly.edu>
2026-05-20 18:16:21 +0000

    Create first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,23 @@
+/*
+    My first C program.
+    I will finish it on Wednesday
+
+*/
+
+/* Multiline comment */
+
+// one-line comment 
+
+
+// Preprocessor directive (starts with #)
+// stdio - standard input output
+// .h - header file
+#include <stdio.h>
+
+// Create the main function
+// <return type> <name> (<parameters>)
+int main(void) {
+    printf("Welcome\nto\nCOP2271!\n"); // \n - new line (escape character)
+    printf("Are\nyou\nready\nto\ncode?\n");
+    return 0;
+}
